4e7676498c0c960bbc393e5db00313f5fdfca21b
pavithrachamala <56586609+pavithrachamala@users.noreply.github.com>
2019-11-29 11:40:07 +0530

    Add files via upload

---
+++ Customer_Application/src/com/custApp/testing/TestEmpController.java
@@ -0,0 +1,35 @@
+package com.custApp.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestEmpController {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@After
+	public void tearDown() throws Exception {
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+
+}
